25f716117bc80f3344c0c70ae1a0e33efdab867c
Stephen Wilde <kink@mandys.house>
2023-01-23 09:38:20 +0000

    postcss plugin update

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,11 @@
 module.exports = {
     plugins: {
+        'postcss-import': {},
+        'tailwindcss/nesting': {},
         tailwindcss: {},
-        autoprefixer: {}
+        autoprefixer: {},
+        cssnano: {
+            preset: 'default'
+        }
     }
 };
