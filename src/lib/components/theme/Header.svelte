58e1554dc30e1fe48fe280236227cdc1c68dcfcd
Stephen Wilde <kink@mandys.house>
2023-01-28 03:46:01 +0000

    change header

--- src/lib/components/theme/Header.svelte
+++ src/lib/components/theme/Header.svelte
@@ -11,20 +11,13 @@
 <div class="tw-bg-black dark:tw-bg-grey">
     <div class="tw-container tw-grid tw-grid-cols-2 tw-items-center tw-gap-4 tw-py-9">
         <Logo textColor="tw-text-white" />
-        <div class="tw-col-start-2 tw-flex tw-justify-end">
+        <div class="tw-col-start-2 tw-flex tw-justify-end tw-space-x-4">
             <div class="tw-hidden lg:tw-block">
                 <Links />
             </div>
-            <div class="tw-block lg:tw-hidden">
+            <div>
                 <Mobile />
             </div>
-        </div>
-    </div>
-    <div
-        class="tw-hidden tw-border-b tw-border-t tw-border-grey tw-py-4 dark:tw-border-black lg:tw-block"
-    >
-        <div class="tw-container">
-            <Navigation />
         </div>
     </div>
 </div>
