2455bc4539a1f1db76ce52e9d54f5273065165c0
Stephen Wilde <kink@mandys.house>
2023-02-06 15:42:50 +0000

    fix build error

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,36 @@
+import { storyblokInit, apiPlugin, useStoryblokApi } from '@storyblok/svelte';
+
+import Feature from '$lib/components/storyblok/Feature.svelte';
+import Grid from '$lib/components/storyblok/Grid.svelte';
+import Page from '$lib/components/storyblok/Page.svelte';
+import Teaser from '$lib/components/storyblok/Teaser.svelte';
+
+// TODO: Move this to layout file
+storyblokInit({
+    accessToken: 'ioaTMb5iyqfQsmfCk2BNYQtt',
+    use: [apiPlugin],
+    components: {
+        feature: Feature,
+        grid: Grid,
+        page: Page,
+        teaser: Teaser
+    }
+});
+
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
     // console.log('+page.server.ts');
 
     try {
+        const storyblokApi = useStoryblokApi();
+
+        const storyData = await storyblokApi.get('cdn/stories/home', {
+            version: 'draft'
+        });
+
         return {
-            pageServerResponse: '+page.server.ts'
+            pageServerResponse: '+page.server.ts',
+            story: storyData.data.story
         };
     } catch {
         return {
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,33 +1,8 @@
-import { storyblokInit, apiPlugin, useStoryblokApi } from '@storyblok/svelte';
-
-import Feature from '$lib/components/storyblok/Feature.svelte';
-import Grid from '$lib/components/storyblok/Grid.svelte';
-import Page from '$lib/components/storyblok/Page.svelte';
-import Teaser from '$lib/components/storyblok/Teaser.svelte';
-
-// TODO: Move this to layout file
-storyblokInit({
-    accessToken: 'hbMtGqNYiGnjhZVRegC8tQtt',
-    use: [apiPlugin],
-    components: {
-        feature: Feature,
-        grid: Grid,
-        page: Page,
-        teaser: Teaser
-    }
-});
-
 /** @type {import('./$types').PageLoad} */
 export async function load(data: { data: JSON }) {
     // console.log('+page.ts');
 
     const serverData = data?.data;
-
-    const storyblokApi = useStoryblokApi();
-
-    const storyData = await storyblokApi.get('cdn/stories/home', {
-        version: 'draft'
-    });
 
     // console.log('STORY DATA', storyData);
 
@@ -37,7 +12,6 @@ export async function load(data: { data: JSON }) {
         page: {
             title: 'Page Title',
             content: 'Page content'
-        },
-        story: storyData.data.story
+        }
     };
 }
