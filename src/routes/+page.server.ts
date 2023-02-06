f91797a25070a283006f812bcec9e4c985462f65
Stephen Wilde <kink@mandys.house>
2023-02-06 12:43:16 +0000

    remove sanity

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,39 +1,10 @@
-import { SECRET_SANITY_URL } from '$env/static/private';
-
-import { homeGQL } from '$lib/graphql/homepage.js';
-
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
     try {
         console.log('+page.server.ts');
 
-        const sanityURL = SECRET_SANITY_URL;
-
-        const sanityQuery = homeGQL;
-
-        // TODO: Check for GQL errors, currently we're only checking for HTTP level errors
-        const sanityData = await fetch(sanityURL, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                query: sanityQuery
-            })
-        }).then((res) => {
-            if (res.status !== 200) {
-                return {
-                    status: res.status,
-                    body: new Error(`Sanity Error ${res.status}`)
-                };
-            }
-
-            return res.json();
-        });
-
-        console.log('SANITY DATA', sanityData);
-
         return {
-            pageServerResponse: '+page.server.ts',
-            sanity: sanityData.data
+            pageServerResponse: '+page.server.ts'
         };
     } catch {
         // const httpError = err as { status: number; message: string };
